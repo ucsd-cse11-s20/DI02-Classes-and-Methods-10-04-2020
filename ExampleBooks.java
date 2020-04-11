ce6fbd16a18f5664deff0cb1dfc2eb82bcfa46c5
Kasra Ferdowsifard <kferdowsifard@gmail.com>
2020-04-10 17:05:39 -0700

    Added code and whiteboard

--- ExampleBooks.java
+++ ExampleBooks.java
@@ -1,5 +1,49 @@
+class Author {
+    String name;
+    String fullName;
+
+    Author(String name, String fullName) {
+        this.name = name;
+        this.fullName = fullName;
+    }
+
+    String toText() {
+        return this.fullName;
+    }
+}
+
 class Book {
     String title;
-    String author;
+    Author author;
     int price;
+
+    // Constructor
+    Book(String title, Author author, int price) {
+        // this.fieldName = fieldName;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    String truncateTitle(int length) {
+        if (this.title.length() <= length) {
+            return this.title;
+        } else {
+            return this.title.substring(0, length) + "...";
+        }
+    }
+
+    String toText() {
+        return this.author.toText() + ": " + this.truncateTitle(12);
+    }
+}
+
+class ExampleBooks {
+    Author tolkien = new Author("JRR Tolkien", "John Ronald Reuel Tolkien");
+
+    Book theTwoTowers = new Book("The Two Towers", tolkien, 8);
+    Book theReturnOfTheKing = new Book("The Return of the King", tolkien, 8);
+
+    String authorText = this.tolkien.toText();
+    String theTwoTowersText = this.theTwoTowers.toText();
 }
